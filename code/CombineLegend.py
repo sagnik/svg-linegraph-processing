8bc33219b4a926d2a3fef3000f2cfbe8ee4d0eb6
sagnik <sagnik1987@gmail.com>
2016-02-18 00:23:43 -0500

    the SVGs produced by our system and the PNGS produced by allenAi doesn't have the same shape. Also, the JSON file contains the BB of the words wrt page origin. Co-ordinate system needs to be transformed by first translating by the ImageBB in the JSON and then scaling by SVG Width/ PNG Width (which should be the same as SVG Height/ PNG Height)

--- code/CombineLegend.py
+++ code/CombineLegend.py
@@ -1,10 +1,22 @@
 import os, sys
 import json
+from PIL import Image
+import numpy as np
 
+TOLERANCE=2
+
+def isHorizontal(w1,w2):
+    b1=w1['TextBB']
+    b2=w2['TextBB']
+    return abs(b2[1]-b1[1])<TOLERANCE and abs(b2[3]-b1[3])<TOLERANCE
+
+def imagePixelExists(w1,w2,imLoc):
+    im=np.asarray(Image.open(imLoc)
+      
 def mergeScores(lw,lWs,imageLoc):
     mWList=[]
     for w in lWs:
-        if notHorizontal(lw,w):
+        if not isHorizontal(lw,w):
             mWList.append((w,0))
         elif imagePixelExists(lw,w,imageLoc):
             mWList.append((w,0))
@@ -15,16 +27,22 @@ def mergeScores(lw,lWs,imageLoc):
 def combineLegend(jsonLoc,ImageLoc):
     #lW=legend Word
     lWs=[x for x in open(jsonLoc)['ImageText'] if x['TextLabel']=='legend']
-    i=0
-    while i<len(lWs):
-        lW=lWs[i]
-        print"checking word",i,"out of",len(lWs),"words"
-        i+=1
-        pWs=mergeScores(lW,list(set(lWs)-set([lW])),ImageLoc)
-        mergedlW=merge(lW,pWs)
+    finalLegends=[]
+    while len(lWs)>0:
+        lW=lWs[0]
+        lWs=lWs[1:]
+        pWs=mergeScores(lW,removeWord(lW,lWs),ImageLoc)
+        mergedlW,mergeWord=merge(lW,pWs)
         if mergedlW!=None:
+            lWs=removeWord(mergeWord,lWs)
             lWs.append(mergedlW)
-        
+            print "merged, now number of words: ",len(lWs)
+        else:
+            finalLegends.append(lW)
+    
+    for lW in lWs:
+            finalLegends.append(lW)
+          
               
           
 
--- code/CreateLegendWordsTagged.py
+++ code/CreateLegendWordsTagged.py
@@ -1,6 +1,8 @@
+from __future__ import division
 from PIL import Image,ImageDraw
 import json
 import os,sys
+import xml.etree.cElementTree as ET
 
 def main():
     ''' 
@@ -12,22 +14,47 @@ def main():
     #for a single file in the "code" directory, just for testing.
     orgPngDir="./"
     wcPngDir="./"
-    subtractImageBB=True    
+    subtractImageBB=True 
 
     jsonLoc=sys.argv[1]
+    js=json.load(open(jsonLoc))
+ 
+    svgLoc=orgPngDir+os.path.split(jsonLoc)[1][:-9]+".svg"
+    tree = ET.ElementTree(file=svgLoc)
+    root = tree.getroot()
+    svgH=float(root.get('height'))
+    svgW=float(root.get('width'))
+
+    translateString=root.findall("{http://www.w3.org/2000/svg}g")[0].get('transform') #there's going to be exactly one child
+    tXSVG=abs(round(float(translateString.split("(")[1].split(",")[0])))
+    tYSVG=abs(round(float(translateString.split("(")[1].split(",")[1].split(")")[0])))
+    #print tX,tY,translateString
     
-    im=Image.open(orgPngDir+os.path.split(jsonLoc)[1][:-9]+".png") 
+            
+    im=Image.open(orgPngDir+os.path.split(jsonLoc)[1][:-9]+".png")
+
+    tX=js['ImageBB'][0]
+    tY=js['ImageBB'][1]
+    
+    pngW=js['ImageBB'][2]-js['ImageBB'][0]
+    pngH=js['ImageBB'][3]-js['ImageBB'][1]
+
+    if abs(pngW/svgW - pngH/svgH)>0.1:  
+        print "ratios are wrong, exiting"
+        sys.exit(1)
+
+    tR=svgW/pngW#transform ratio 
+
     draw = ImageDraw.Draw(im)
 
-    js=json.load(open(jsonLoc))
     for imt in js['ImageText']:
         if imt['TextLabel']=='legend':
-            #print imt['Text'],imt['TextBB']
-            if subtractImageBB:
+           if subtractImageBB:
                 a=imt['TextBB']
-                b=[a[0]-js['ImageBB'][0],a[1]-js['ImageBB'][1],a[2]-js['ImageBB'][0],a[3]-js['ImageBB'][1]]
+                b=[(a[0]-tX)*tR,(a[1]-tY)*tR,(a[2]-tX)*tR,(a[3]-tY)*tR]
+                #print imt['Text'],imt['TextBB'],b
                 draw.rectangle(b,outline="blue")
-            else:
+           else:
                 draw.rectangle(imt['TextBB'],outline="blue")
     
     im.save(wcPngDir+os.path.split(jsonLoc)[1][:-5]+".png")     
