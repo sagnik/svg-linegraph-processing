0e8a5a3be4a541eff3d6aef11774c38c30501284
sagnik <sagnik1987@gmail.com>
2016-02-20 21:48:30 -0500

    curve alignment for single value dictionaries working

--- code/CurveLegendAssociation.py
+++ code/CurveLegendAssociation.py
@@ -7,42 +7,64 @@ import json
 from skimage.io import imread
 from skimage.color import rgb2hsv
 import numpy as np
-
+from PIL import Image
 #def associateCurve(jsonLoc,svgLoc)
+from matplotlib import pyplot as plt
+import pylab 
 
 def createCurves(svgLoc):
     separateCurves(svgLoc)
 
+def show_img(img):
+     width = img.shape[1]/75.0
+     height = img.shape[0]*width/img.shape[1]
+     f = plt.figure(figsize=(width, height))
+     plt.imshow(img)
+     pylab.show()
+
 def createPng(svgLoc,W,H):
-    img = cairo.ImageSurface(cairo.FORMAT_RGB24, int(W),int(H))
+    img = cairo.ImageSurface(cairo.FORMAT_ARGB32, int(W),int(H))
     ctx = cairo.Context(img)
     handle= rsvg.Handle(None, str(open(svgLoc).read()))
     handle.render_cairo(ctx)
     img.write_to_png(svgLoc[:-4]+".png")
+    png = Image.open(svgLoc[:-4]+".png")
+    png.load() # required for png.split()
+
+    nonalphaPng = Image.new("RGB", png.size, (255, 255, 255))
+    nonalphaPng.paste(png, mask=png.split()[3])
+    nonalphaPng.save(svgLoc[:-4]+".png") 
+
 
 def curveScore(l,curve):
     lb=l['TextBB']
-    elbl=[lb[0]-200,lb[1],lb[0]-5,lb[3]]
-    elbr=[lb[0]+5,lb[1],lb[2]+200,lb[3]]
+    elbl=[lb[0]-20,lb[1],lb[0],lb[3]]
+    elbr=[lb[0],lb[1],lb[2]+20,lb[3]]
     cindex=curve[0] 
     cdata=curve[1]
     #img=cdata[elb[1]:elb[3],elb[0]:elb[2]]
     imgl=cdata[elbl[1]:elbl[3],elbl[0]:elbl[2]]
     imgr=cdata[elbr[1]:elbr[3],elbr[0]:elbr[2]]
     
+    #print cindex,l['Text']
+    #show_img(imgl)
+    #show_img(imgr)    
     #points from the rectangle to the left and right of the legend word that are not white or black pixels. 
     lnzps=np.where(np.logical_and(np.greater_equal(imgl[:,:,1],0.06),np.greater_equal(imgl[:,:,2],0.1))) 
     rnzps=np.where(np.logical_and(np.greater_equal(imgr[:,:,1],0.06),np.greater_equal(imgr[:,:,2],0.1)))
     
     if len(lnzps[0])==0 and len(rnzps[0])==0: #this means for this legend, we did not find a single pixel from the 
     #curve that is to the left or right of it.
+        #print l['Text'],"has no points to the left or right for",cindex
         return (None,None)
     elif len(lnzps[0])!=0 and len(rnzps[0])==0:
+        #print l['Text'],"has curve",cindex,"to the left of it, distance:",100-np.sort(lnzps[0])[-1]
         return(cindex,100-np.sort(lnzps[0])[-1])
     elif len(lnzps[0])==0 and len(rnzps[0])!=0:
+        #print l['Text'],"has curve",cindex,"to the right of it, distance:",np.sort(rnzps[0])[0]
         return(cindex,np.sort(rnzps[0])[0])
     else: #this means, some points from this curve belongs to both left and the right of the legend. That is improbable.
-        print "Something wrong"
+        print "Something wrong, a single curve has pixels on both sides of the image "
         return (None,None)
       
 def legendCurveDictionary(legends,curveLocs):
